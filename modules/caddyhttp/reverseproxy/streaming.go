17f983335d5e5b93803c46f2ab184a44849b2482
Matthew Holt <mholt@users.noreply.github.com>
2022-07-16 23:40:56 -0600

    Use httpguts, duh

--- modules/caddyhttp/reverseproxy/streaming.go
+++ modules/caddyhttp/reverseproxy/streaming.go
@@ -27,6 +27,7 @@ import (
 	"time"
 
 	"go.uber.org/zap"
+	"golang.org/x/net/http/httpguts"
 )
 
 func (h Handler) handleUpgradeResponse(logger *zap.Logger, rw http.ResponseWriter, req *http.Request, res *http.Response) {
@@ -243,8 +244,8 @@ func writeCloseControl(conn io.Writer) error {
 // isWebsocket returns true if r looks to be an upgrade request for WebSockets.
 // It is a fairly naive check.
 func isWebsocket(r *http.Request) bool {
-	return strings.Contains(strings.ToLower(r.Header.Get("Connection")), "upgrade") &&
-		strings.Contains(strings.ToLower(r.Header.Get("Upgrade")), "websocket")
+	return httpguts.HeaderValuesContainsToken(r.Header["Connection"], "upgrade") &&
+		httpguts.HeaderValuesContainsToken(r.Header["Upgrade"], "websocket")
 }
 
 // openConnection maps an open connection to
